f2e0c19dc240b45609abb5c1fd1b1230bd9c7aa8
David Bower <davidbower931@gmail.com>
2025-10-24 14:06:45 -0400

    main change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Frankenmolder")
 
-label = tk.Label(root, text="🧠  Frankenmolder  ⚙️", font=("Helvetica", 24))
+label = tk.Label(root, text="Frankenmolder is running", font=("Helvetica", 24))
 label.pack(padx=40, pady=40)
 
 root.mainloop()
